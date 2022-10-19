b8822fb17ca43126c6aa5cc4ea56cbaede0d3a8e
Mahdi Ataei <54065538+mahdiataie@users.noreply.github.com>
2022-10-19 16:59:30 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { useState } from "react";
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  // const dummyMovies = [
+  //   {
+  //     id: 1,
+  //     title: 'Some Dummy Movie',
+  //     openingText: 'This is the opening text of the movie',
+  //     releaseDate: '2021-05-18',
+  //   },
+  //   {
+  //     id: 2,
+  //     title: 'Some Dummy Movie 2',
+  //     openingText: 'This is the second opening text of the movie',
+  //     releaseDate: '2021-05-19',
+  //   },
+  // ];
+  const [movies, setMovies] = useState([]);
+   async function fetchMoviesHandler() {
+   const response = await fetch("https://swapi.dev/api/films/")
+  const data = await response.json();
+        const transformMovies = data.results.map((movieData) => {
+          return {
+            id: movieData.episode_id,
+            title: movieData.title,
+            openingText: movieData.opening_crawl,
+            releaseDate: movieData.release_date,
+          };
+        });
+        setMovies(transformMovies);
+  }
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
